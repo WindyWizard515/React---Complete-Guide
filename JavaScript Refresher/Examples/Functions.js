e9012023cffeeee05713c852a9353403c66718a9
Paul Hudson <paul@paul.localdomain>
2025-03-08 17:49:13 -0600

    First JS exercise - Working With Functions

---
+++ JavaScript Refresher/Examples/Functions.js
@@ -0,0 +1,15 @@
+// Create a function that completes the requirements
+const combine = function(a,b,c) {
+    return a*b/c
+}
+
+// Create an arrow function that completes the requirements
+const combineArrowFunction = (a,b,c) => {
+    return a*b/c
+}
+
+// Run the combine function
+combine(3,2,3) // Outputs 2
+
+// Run the combineArrowFunction function
+combineArrowFunction(3,2,3) // Outputs 2
