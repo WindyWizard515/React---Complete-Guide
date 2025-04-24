64542d72af1970a4cf926ebf01a197e385d92443
Paul Hudson <paul@paul.localdomain>
2025-04-23 20:40:16 -0500

    Added some buttons to the main app page

--- React Essentials/Starting Projects/src/App.jsx
+++ React Essentials/Starting Projects/src/App.jsx
@@ -4,6 +4,7 @@ import { CORE_CONCEPTS } from "./data.js"
 // Import functions
 import Header from "./components/Header/Header.jsx"
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx"
+import TabButton from "./components/TabButton.jsx"
 
 function App() {
   return (
@@ -18,6 +19,14 @@ function App() {
             <CoreConcept{...CORE_CONCEPTS[2]}/>
             <CoreConcept{...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>Components</TabButton>
+            <TabButton>Components</TabButton>
+            <TabButton>Components</TabButton>
+          </menu>
         </section>
       </main>
     </div>
