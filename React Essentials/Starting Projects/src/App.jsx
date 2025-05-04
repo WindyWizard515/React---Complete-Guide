7d24d089689c2cc4f922076505f13d5d0dd2e1a7
Paul Hudson <paul@paul.localdomain>
2025-05-04 13:11:27 -0500

    Updates to App.js

--- React Essentials/Starting Projects/src/App.jsx
+++ React Essentials/Starting Projects/src/App.jsx
@@ -1,5 +1,5 @@
 // Import data
-import { CORE_CONCEPTS } from "./data.js"
+import { EXAMPLES, CORE_CONCEPTS } from "./data.js"
 
 // Import functions
 import Header from "./components/Header/Header.jsx"
@@ -10,7 +10,7 @@ import TabButton from "./components/TabButton.jsx"
 import { useState } from "react"
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please select a tab")
+  const [selectedTopic, setSelectedTopic] = useState("components")
 
   function handleSelect(button) {
     // button => 'components', 'jsx', 'props', or 'state'
@@ -40,7 +40,11 @@ function App() {
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
             <div id="tab-content">
-              
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
             </div>
         </section>
       </main>
