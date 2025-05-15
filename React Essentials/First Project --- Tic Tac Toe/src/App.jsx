5f394ddd2ec2ebf13c4e3888ef50be1582843bb5
Paul Hudson <paul@paul.localdomain>
2025-05-14 20:10:40 -0500

    Made the game able to be played

--- React Essentials/First Project --- Tic Tac Toe/src/App.jsx
+++ React Essentials/First Project --- Tic Tac Toe/src/App.jsx
@@ -1,17 +1,51 @@
+import { useState } from "react"
+
 import Player from "./Components/Player"
 import GameBoard from "./Components/GameBoard"
+import Log from "./Components/Log"
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X")
+  const [gameTurns, setGameTurns] = useState([])
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X")
+    setGameTurns(prevTurns => {
+      let currentPlayer = "X"
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer= "0"
+      }
+
+      const updatedTurns = [
+        { square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns]
+    })
+
+    return updatedTurns
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-        <Player initialName="Player 1" symbol="X"/>
-        <Player initialName="Player 2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+          turns={gameTurns}
+        />
       </div>
-      LOG
+      <LOG />
     </main>
   )
 }
