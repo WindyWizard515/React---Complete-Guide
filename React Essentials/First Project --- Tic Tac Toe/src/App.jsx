8a88138dfd42b64affff4ac562fe6864ad1ce562
Paul Hudson <paul@paul.localdomain>
2025-05-27 13:35:06 -0500

    Finished the Project

--- React Essentials/First Project --- Tic Tac Toe/src/App.jsx
+++ React Essentials/First Project --- Tic Tac Toe/src/App.jsx
@@ -3,9 +3,21 @@ import { useState } from "react"
 import Player from "./Components/Player.jsx"
 import GameBoard from "./Components/GameBoard.jsx"
 import Log from "./Components/Log.jsx"
+import GameOver from "./Components/GameOver.jsx"
 import { WINNING_COMBINATIONS } from "../winning-combinations.js"
 
-function getActivePlayer(gameTurns) {
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2"
+}
+
+const INITIAL_GAME_BOARD=[
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+]
+
+function deriveActivePlayer(gameTurns) {
     let currentPlayer = "X"
 
     if (gameTurns.length > 0 && gameTurns[0].player === "X") {
@@ -15,18 +27,53 @@ function getActivePlayer(gameTurns) {
     return currentPlayer
 }
 
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])]
+
+  for (const turn of gameTurns) {
+      const { square, player } = turn
+      const { row, col } = square
+
+      gameBoard[row][col] = player
+  }
+
+  return gameBoard
+}
+
+function deriveWinner(gameBoard, players) {
+  let winner
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquare =
+      gameBoard[combination[0].row][combination[0].column]
+    const secondSquare =
+      gameBoard[combination[1].row][combination[1].column]
+    const thirdSquare =
+      gameBoard[combination[2].row][combination[2].column]
+
+    if (
+      firstSquare &&
+      firstSquare === secondSquare &&
+      firstSquare === thirdSquare
+    ) {
+      winner = players[firstSquare]
+    }
+  }
+  return winner
+}
+
 function App() {
+  const [players, setPlayers] = useState(PLAYERS)
   const [gameTurns, setGameTurns] = useState([])
 
-  const activePlayer = getActivePlayer(gameTurns)
+  const activePlayer = deriveActivePlayer(gameTurns)
+  const gameBoard = deriveGameBoard(gameTurns)
+  const winner = deriveWinner(gameBoard, players)
+  const hasDraw = gameTurns.length === 9 && !winner
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
-      let currentPlayer = getActivePlayer(prevTurns)
-      
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer= "O"
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns)
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns]
@@ -36,24 +83,40 @@ function App() {
 
   }
 
+  function handleRestart() {
+    setGameTurns([])
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      }
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
+          board={gameBoard}
         />
       </div>
       <Log turns={gameTurns}/>
