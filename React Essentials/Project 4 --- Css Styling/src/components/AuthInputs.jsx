d22b4e9a06f0627419abaf0aaf52e66d792b4fe4
Paul Hudson <paul@paul.localdomain>
2025-06-04 14:55:51 -0500

    Used inline styling for the invalid input values

--- React Essentials/Project 4 --- Css Styling/src/components/AuthInputs.jsx
+++ React Essentials/Project 4 --- Css Styling/src/components/AuthInputs.jsx
@@ -24,21 +24,23 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
             style={{
-              backgroundColor: emailNotValid ? '#ef4444' : '#d1d5db'
+              backgroundColor: emailNotValid ? '#ef4444' : '#d1d5db',
             }}
             // className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</label>
           <input
             type="password"
-
+            style={{
+              backgroundColor: emailNotValid ? '#ef4444' : '#d1d5db',
+            }}
             // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
