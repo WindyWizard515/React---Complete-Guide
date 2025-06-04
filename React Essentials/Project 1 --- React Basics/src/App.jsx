7cee837449a6faaf77a89171c9078d5ca4a5548b
Paul Hudson <paul@paul.localdomain>
2025-06-04 14:12:23 -0500

    Final Commit

---
+++ React Essentials/Project 1 --- React Basics/src/App.jsx
@@ -0,0 +1,58 @@
+// Import data
+import { EXAMPLES, CORE_CONCEPTS } from "./data.js"
+
+// Import functions
+import Header from "./components/Header/Header.jsx"
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx"
+import TabButton from "./components/TabButton.jsx"
+
+// Import from react
+import { useState } from "react"
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState()
+
+  function handleSelect(button) {
+    // button => 'components', 'jsx', 'props', or 'state'
+    setSelectedTopic(button)
+
+    console.log(selectedTopic)
+  }
+
+  let tabContent = <p>Please select a topic</p>
+
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>{EXAMPLES[selectedTopic].code}</code>
+    </pre>
+  </div>
+}
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title}{...conceptItem} />)}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic === "components"} onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === "props"} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === "state"} onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+              {selectedTopic===undefined ? <p>Please select a topic</p> : tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
