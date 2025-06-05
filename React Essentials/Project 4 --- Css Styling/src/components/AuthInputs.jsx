74abd0beffe1e19d6b43e01aee176d63485f9630
Paul Hudson <paul@paul.localdomain>
2025-06-05 10:24:57 -0500

    Fixed the label and input boxes not being dynamically styled

--- React Essentials/Project 4 --- Css Styling/src/components/AuthInputs.jsx
+++ React Essentials/Project 4 --- Css Styling/src/components/AuthInputs.jsx
@@ -8,8 +8,28 @@ const ControlDiv = styled.div`
   margin-bottom: 1.5rem;
 `
 
-const Label = styled.label
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({ $invalid }) => $invalid ? "#f87171" : "#6b7280"};
+ `
 
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${({ $invalid }) => $invalid ? "#fed2d2" : "#d1d5db"};
+  color: ${({ $invalid }) => $invalid ? "#ef4444" : "#374151"};
+  border: 1px solid ${({ $invalid }) => $invalid ? "#f73f3f" : "transparent"};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
+
+//  className={`label ${emailNotValid ? 'invalid' : ''}`}
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -34,8 +54,9 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlDiv>
         <p>
-          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
-          <input
+          <Label $invalid={emailNotValid}>Email</Label>
+          <Input
+            $invalid={emailNotValid}
             type="email"
             style={{
               backgroundColor: emailNotValid ? '#ef4444' : '#d1d5db',
@@ -45,8 +66,9 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</label>
-          <input
+          <Label $invalid={passwordNotValid}>Password</Label>
+          <Input
+            $invalid={passwordNotValid}
             type="password"
             style={{
               backgroundColor: emailNotValid ? '#ef4444' : '#d1d5db',
