bb88d2ce678a2d87d8114ee21704b9d626731329
Paul Hudson <paul@paul.localdomain>
2025-06-15 18:21:08 -0500

    Made the buttons interactable

--- React Essentials/Project 7 --- Project Management/src/App.jsx
+++ React Essentials/Project 7 --- Project Management/src/App.jsx
@@ -7,14 +7,31 @@ import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   const [projectsState, setProjectsState] = useState({
     currentAction: "nothing-selected",
-    selectedProject: undefined,
+    selectedProjectId: undefined,
     projects: []
   })
 
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if ( projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      {}<NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
